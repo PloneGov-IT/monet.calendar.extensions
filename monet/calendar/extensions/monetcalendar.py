1af32a46309d80565926719bafa8a989b272abbc
redturtle <redturtle@6a6e2cd2-7798-44f2-a3d3-4feb5ca87cd5>
2010-06-03 14:42:43 +0000

    overrides calendar portlet

--- monet/calendar/extensions/monetcalendar.py
+++ monet/calendar/extensions/monetcalendar.py
@@ -7,6 +7,9 @@ class Assignment(Assignment):
 class Renderer(Renderer):
     """"""
     _template = ViewPageTemplateFile('monetcalendar.pt')
-
+    
+    def getDateString(self,daynumber):
+        return '%s-%s-%s' % (self.year, self.month, daynumber)
+        
 class AddForm(AddForm):
     """"""
