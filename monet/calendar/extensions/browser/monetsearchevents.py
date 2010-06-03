dd3c2e2977907f7372487b3fefaf971c24d6c89b
redturtle <redturtle@6a6e2cd2-7798-44f2-a3d3-4feb5ca87cd5>
2010-06-03 16:31:25 +0000

    path search

--- monet/calendar/extensions/browser/monetsearchevents.py
+++ monet/calendar/extensions/browser/monetsearchevents.py
@@ -7,43 +7,12 @@ from datetime import datetime, timedelta
 from monet.calendar.extensions import eventMessageFactory as _
 from Products.statusmessages.interfaces import IStatusMessage
 from zope.component import getMultiAdapter
+from monet.calendar.extensions.browser.usefulforsearch import UsefulForSearchEvents
 
-class MonetSearchEvents(BrowserView):
+class MonetSearchEvents(BrowserView,UsefulForSearchEvents):
     
     def __init__(self, context, request):
         BrowserView.__init__(self, context, request)
-        
-    def getSubSiteParentFolder(self):
-        """Return the first parent folder found that implements the interface IMonetCalendarSearchRoot"""
-        for parent in aq_chain(self):
-            if IMonetCalendarSearchRoot.providedBy(parent):
-                return parent
-        return None
-        
-    def getCalendarSection(self,subsite):
-        """Return the first folder found that implements the interface IMonetCalendarSection"""
-        pcatalog = getToolByName(self, 'portal_catalog')
-        query = {}
-        query['object_provides'] = IMonetCalendarSection.__identifier__
-        query['path'] = '/'.join(subsite.getPhysicalPath())
-        query['sort_on'] = 'getObjPositionInParent'
-        brains = pcatalog.searchResults(**query)
-        if brains:
-            return brains[0]
-        return None
-        
-    def getCalendarSectionParentNoSubSite(self):
-        """Return the first folder found in the site (no sub-site) that implements the interface IMonetCalendarSection"""
-        portal=getToolByName(self, 'portal_url').getPortalObject()
-        pcatalog = getToolByName(self, 'portal_catalog')
-        query = {}
-        query['object_provides'] = IMonetCalendarSection.__identifier__
-        query['path'] = {'query':'/'.join(portal.getPhysicalPath()),'depth':1}
-        query['sort_on'] = 'getObjPositionInParent'
-        brains = pcatalog.searchResults(**query)
-        if brains:
-            return brains[0]
-        return None
     
     def getEventsInParent(self,parent):
         """Return all events found in the parent folder"""
