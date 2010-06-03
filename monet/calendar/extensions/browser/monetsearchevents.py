18453ffb25afe20bac1e83c6852dccd9947b006f
redturtle <redturtle@6a6e2cd2-7798-44f2-a3d3-4feb5ca87cd5>
2010-06-03 11:52:25 +0000

    check event date

--- monet/calendar/extensions/browser/monetsearchevents.py
+++ monet/calendar/extensions/browser/monetsearchevents.py
@@ -5,6 +5,8 @@ from Acquisition import aq_chain
 from Products.CMFCore.utils import getToolByName
 from datetime import datetime, timedelta
 from monet.calendar.extensions import eventMessageFactory as _
+from Products.statusmessages.interfaces import IStatusMessage
+from zope.component import getMultiAdapter
 
 class MonetSearchEvents(BrowserView):
     
@@ -59,31 +61,35 @@ class MonetSearchEvents(BrowserView):
         form = self.request.form
         
         if form.get('day'):
-            return {'errors':0,
-                    'message_error':'',
-                    'events':events}
+            return events
         elif self.notEmptyArgumentsDate(form.get('fromDay'),form.get('fromMonth'),form.get('fromYear')) or self.notEmptyArgumentsDate(form.get('toDay'),form.get('toMonth'),form.get('toYear')):
             date_from = self.writeDate(form.get('fromDay'),form.get('fromMonth'),form.get('fromYear'))
             date_to = self.writeDate(form.get('toDay'),form.get('toMonth'),form.get('toYear'))
             if not date_from or not date_to:
-                return {'errors':1,
-                        'message_error':_(u'label_failed_arguments', default=u'The date arguments are not valid, re-enter the dates please'),
-                        'events':[]}
-            elif self.checkInvalidDate(date_from,date_to):
-                return {'errors':1,
-                        'message_error':_(u'label_failed_interval', default=u'The second data parameter (TO) must be greater than or equal to the first data parameter (FROM), so that the range of days specified is not negative. Furthermore, the search of events must be less than 30 days.'),
-                        'events':[]}
+                message_error = _(u'label_failed_arguments', default=u'The date arguments are not valid, re-enter the dates, please.')
+                IStatusMessage(self.request).addStatusMessage(message_error,type="error")
+                url = getMultiAdapter((self.context, self.request),name='absolute_url')()
+                self.request.response.redirect(url + '/@@monetsearchevents')
+                return 
+            elif self.checkInvalidDateGreaterThan(date_from,date_to):
+                message_error = _(u'label_failed_gtinterval', default=u'The second data parameter (TO) must be greater than or equal to the first data parameter (FROM), so that the range of days specified is not negative. Re-enter the dates, please.')
+                IStatusMessage(self.request).addStatusMessage(message_error,type="error")
+                url = getMultiAdapter((self.context, self.request),name='absolute_url')()
+                self.request.response.redirect(url + '/@@monetsearchevents')
+                return
+            elif self.checkInvalidDateInterval(date_from,date_to):
+                message_error = _(u'label_failed_interval', default=u'The search of events must be less than 30 days. Re-enter the dates, please.')
+                IStatusMessage(self.request).addStatusMessage(message_error,type="error")
+                url = getMultiAdapter((self.context, self.request),name='absolute_url')()
+                self.request.response.redirect(url + '/@@monetsearchevents')
+                return
             else:
                 filtered_events = self.filterEventsByDate(events,date_from,date_to)
-                return {'errors':0,
-                        'message_error':'',
-                        'events':filtered_events}
+                return filtered_events
         else:
             date_from = date_to = datetime.now().date()
             filtered_events = self.filterEventsByDate(events,date_from,date_to)
-            return {'errors':0,
-                    'message_error':'',
-                    'events':filtered_events}
+            return filtered_events
         
     def notEmptyArgumentsDate(self,day,month,year):
         """Check the date viewlets's parameters"""
@@ -100,9 +106,16 @@ class MonetSearchEvents(BrowserView):
         except StandardError:
             return None
         
-    def checkInvalidDate(self,date_from,date_to):
+    def checkInvalidDateGreaterThan(self,date_from,date_to):
         """Check the dates DAL AL"""
-        if date_to >= date_from and not (date_to - date_from).days > 30:
+        if date_to >= date_from:
+            return False
+        else:
+            return True
+        
+    def checkInvalidDateInterval(self,date_from,date_to):
+        """Check the dates DAL AL"""
+        if not (date_to - date_from).days > 30:
             return False
         else:
             return True
