ee222b770c417f3b690d8a0e02dca80b1e350822
redturtle <redturtle@6a6e2cd2-7798-44f2-a3d3-4feb5ca87cd5>
2010-06-04 07:13:43 +0000

    chache of calendar

--- monet/calendar/extensions/monetcalendar.py
+++ monet/calendar/extensions/monetcalendar.py
@@ -1,6 +1,7 @@
 from plone.app.portlets.portlets.calendar import Assignment,Renderer,AddForm
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from monet.calendar.extensions.browser.usefulforsearch import UsefulForSearchEvents
+from plone.memoize.compress import xhtml_compress
 
 class Assignment(Assignment):
     """"""
@@ -11,6 +12,10 @@ class Renderer(Renderer,UsefulForSearchEvents):
     
     def getDateString(self,daynumber):
         return '%s-%s-%s' % (self.year, self.month, daynumber)
-        
+
+    def render(self):
+        return xhtml_compress(self._template())
+
+  
 class AddForm(AddForm):
     """"""
