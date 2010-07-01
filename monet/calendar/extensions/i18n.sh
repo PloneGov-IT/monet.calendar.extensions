d2be4e4e7890417d8b702e941726c65ba6d2df70
redturtle <redturtle@6a6e2cd2-7798-44f2-a3d3-4feb5ca87cd5>
2010-07-01 13:23:17 +0000

    manual pot

--- monet/calendar/extensions/i18n.sh
+++ monet/calendar/extensions/i18n.sh
@@ -3,7 +3,7 @@
 DOMAIN='monet.calendar.extensions'
 
 i18ndude rebuild-pot --pot locales/${DOMAIN}.pot --create ${DOMAIN} .
-
+i18ndude rebuild-pot --pot locales/${DOMAIN}.pot --merge locales/${DOMAIN}-manual.pot --create ${DOMAIN} .
 i18ndude sync --pot locales/${DOMAIN}.pot locales/*/LC_MESSAGES/${DOMAIN}.po
 
 # Compile po files
@@ -12,3 +12,4 @@ for lang in $(find locales -mindepth 1 -maxdepth 1 -type d); do
         msgfmt -o $lang/LC_MESSAGES/${DOMAIN}.mo $lang/LC_MESSAGES/${DOMAIN}.po
     fi
 done
+
