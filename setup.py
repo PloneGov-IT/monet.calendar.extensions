3b9464e5b424dccca93f875e961f305ca85d30ec
redturtle <redturtle@6a6e2cd2-7798-44f2-a3d3-4feb5ca87cd5>
2011-01-14 15:46:52 +0000

    Moltissime modifiche per migliorare il codice E qualche per la chiusura del #118

--- setup.py
+++ setup.py
@@ -10,8 +10,9 @@ setup(name='monet.calendar.extensions',
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
-        "Programming Language :: Python",
         "Framework :: Plone",
+        "Programming Language :: Python",
+        'License :: OSI Approved :: GNU General Public License (GPL)',
         ],
       keywords='',
       author='RedTurtle Technology',
