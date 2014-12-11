ff7f29b1bdc4dfcfae34348411e420fedccc91db
Luca Bellenghi <lucabel@gmail.com>
2014-12-11 10:30:07 +0100

    modifiche alla portlet

--- monet/calendar/extensions/i18n.sh
+++ monet/calendar/extensions/i18n.sh
@@ -3,8 +3,8 @@
 DOMAIN='monet.calendar.extensions'
 
 #i18ndude rebuild-pot --pot locales/${DOMAIN}.pot --create ${DOMAIN} .
-i18ndude rebuild-pot --pot locales/${DOMAIN}.pot --merge locales/${DOMAIN}-manual.pot --create ${DOMAIN} .
-i18ndude sync --pot locales/${DOMAIN}.pot locales/*/LC_MESSAGES/${DOMAIN}.po
+~/workspace/www.comune.modena.it/components/plone/bin/i18ndude rebuild-pot --pot locales/${DOMAIN}.pot --merge locales/${DOMAIN}-manual.pot --create ${DOMAIN} .
+~/workspace/www.comune.modena.it/components/plone/bin/i18ndude sync --pot locales/${DOMAIN}.pot locales/*/LC_MESSAGES/${DOMAIN}.po
 
-i18ndude sync --pot i18n/plone-${DOMAIN}.pot i18n/plone-${DOMAIN}-??.po
+~/workspace/www.comune.modena.it/components/plone/bin/i18ndude sync --pot i18n/plone-${DOMAIN}.pot i18n/plone-${DOMAIN}-??.po
 
